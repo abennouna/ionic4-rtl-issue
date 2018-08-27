633320f81ee21f0ccf7f60bcec19468faaa88e7f
Abdelaziz <abennouna@gmail.com>
2018-08-27 11:29:03 +0100

    demonstrate margin/padding issue in RTL mode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private htmlRoot = document.documentElement;
+  currentDirection = this.htmlRoot.getAttribute('dir') || 'ltr';
 
+  toggleDirection() {
+    if (this.currentDirection === 'ltr') {
+      this.currentDirection = 'rtl';
+    } else if (this.currentDirection === 'rtl') {
+      this.currentDirection = 'ltr';
+    }
+
+    this.htmlRoot.setAttribute('dir', this.currentDirection);
+  }
 }
